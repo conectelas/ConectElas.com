61d944f7225ba7fbbd2afc2ef455a8a102c1d19c
Daniela <goulartsdaniela@gmail.com>
2022-03-03 11:31:28 -0200

    Postagem CRUD funcionando

--- BackEnd/conectelas/src/main/java/br/org/conectelas/model/UsuarioModel.java
+++ BackEnd/conectelas/src/main/java/br/org/conectelas/model/UsuarioModel.java
@@ -1,4 +1,5 @@
 package br.org.conectelas.model;
+
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -13,28 +14,31 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-
 @Entity
-@Table(name = "tema")
-public class TemaModel {
-
+@Table(name="tb_usuario")
+public class UsuarioModel {
+	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy =  GenerationType.IDENTITY)
 	private long id;
-
+	
 	@NotBlank
-	@Size(min = 4, max = 20)
-	private String nome;
-
+	@Size(min=5, max=100)
+	private String nomeCompleto;
+	
 	@NotBlank
-	@Size(min = 4, max = 100)
-	private String descricao;
-
-	@OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
-	@JsonIgnoreProperties("tema")
+	@Size(min=10, max=200)
+	private String email;
+	
+	@NotBlank
+	@Size(min=8, max=300)
+	private String senha;
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("usuario")
 	private List<PostagemModel> postagem;
-
-	// Getters and Setters
+	
+	//Getters and Setters
 	public long getId() {
 		return id;
 	}
@@ -43,20 +47,28 @@ public class TemaModel {
 		this.id = id;
 	}
 
-	public String getNome() {
-		return nome;
+	public String getNomeCompleto() {
+		return nomeCompleto;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
 	}
 
-	public String getDescricao() {
-		return descricao;
+	public String getEmail() {
+		return email;
 	}
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 	public List<PostagemModel> getPostagem() {
@@ -66,6 +78,6 @@ public class TemaModel {
 	public void setPostagem(List<PostagemModel> postagem) {
 		this.postagem = postagem;
 	}
-
-
+	
+	
 }
--- BackEnd/conectelas/src/main/java/br/org/conectelas/repository/TemaRepository.java
+++ BackEnd/conectelas/src/main/java/br/org/conectelas/repository/TemaRepository.java
@@ -1,11 +1,13 @@
 package br.org.conectelas.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.org.conectelas.model.TemaModel;
 
-public interface TemaRepository extends JpaRepository<TemaModel, Long>{
-	
-//	@Query("Select * From tema where ")
-	
+public interface TemaRepository extends JpaRepository<TemaModel, Long> {
+
+	public Optional<TemaModel> findByNome(String nome);
+
 }
